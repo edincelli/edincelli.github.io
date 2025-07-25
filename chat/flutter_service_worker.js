5aef0033d02c1f07bea25df56fc49f00ca040010
kusyk <lukaszk133@gmail.com>
2025-07-25 11:22:44 -0600

    fix aion chat

--- chat/flutter_service_worker.js
+++ chat/flutter_service_worker.js
@@ -21,14 +21,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "13a465f7b11116a0a70a39027b4b40dd",
+"flutter_bootstrap.js": "5c0b55b04dde9f7e4abe89a5cead8745",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "1a7cb5d9c1f5a5bc26dfa06f9074dc9c",
 "/": "1a7cb5d9c1f5a5bc26dfa06f9074dc9c",
-"main.dart.js": "bb611de46466c9f658a6f142c2f362ba",
+"main.dart.js": "5b346972d67ce5027f4760129fc56ba2",
 "manifest.json": "109ec18e4f1c7ac7f79873ea180f3643",
 "version.json": "9a05170b839f9950c152ab3740f42a30"};
 // The application shell files that are downloaded before a service worker can
