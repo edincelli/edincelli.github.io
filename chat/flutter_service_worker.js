1acec4aa5cd2af9932b0380aaa148c120b9ef081
kusyk <lukaszk133@gmail.com>
2025-08-11 18:09:17 -0600

    update aion chat (update name and description)

--- chat/flutter_service_worker.js
+++ chat/flutter_service_worker.js
@@ -21,15 +21,15 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "326e3d899b9c013b7d3654e51b3bc219",
+"flutter_bootstrap.js": "c64c52a68b76c076e5fae58808e4ab04",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "1a7cb5d9c1f5a5bc26dfa06f9074dc9c",
-"/": "1a7cb5d9c1f5a5bc26dfa06f9074dc9c",
+"index.html": "4cb5dad57eac2aa56d45002f06e2c42a",
+"/": "4cb5dad57eac2aa56d45002f06e2c42a",
 "main.dart.js": "68efc17b80d7398569a69f4c386f6f36",
-"manifest.json": "109ec18e4f1c7ac7f79873ea180f3643",
+"manifest.json": "bc9534b8044e6e35a5fc4e3ee8eec8a8",
 "version.json": "9a05170b839f9950c152ab3740f42a30"};
 // The application shell files that are downloaded before a service worker can
 // start.
